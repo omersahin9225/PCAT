20fec697858ba506f80c882086a545c98864143a
omersahin9225 <83361242+omersahin9225@users.noreply.github.com>
2021-12-30 20:34:12 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-const fn=function(){
-    console.log('function Working' )
-};
+const express = require('express');
+const path  = require('path');
+
+const app = express();
+
+
+
+
+
+
+//Middlewares
+app.use(express.static('public'))
+
+
+
+
+app.get('/', (req, res)=>{
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+    
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`sunucu ${port} portunda başlatıldı `);
+});
